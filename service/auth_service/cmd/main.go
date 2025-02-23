8e8ca54ff69bae3b166495f879d7d3c534a46331
oak_cassia <gnlckswjd1@gmail.com>
2025-02-23 20:14:17 +0900

    feat : add main

--- service/auth_service/cmd/main.go
+++ service/auth_service/cmd/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"auth_service/config"
+	"auth_service/internal/handler"
+	"auth_service/internal/repository"
+	"auth_service/internal/service"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const port = 10001
+const postMethod = "POST"
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func Method(m string) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != m {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			next(w, r)
+		}
+	}
+}
+
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
 func main() {
-	fmt.Println("이러면 되지")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+	defer cfg.Close()
+
+	userRepo := repository.NewUserRepository(cfg.DB)
+	authService := service.NewAuthService(userRepo, cfg.Redis)
+	authHandler := handler.NewAuthHandler(authService)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hi")) })
+	http.HandleFunc("/register", Chain(authHandler.RegisterHandler, Method(postMethod)))
+	http.HandleFunc("/login", Chain(authHandler.LoginHandler, Method(postMethod)))
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
