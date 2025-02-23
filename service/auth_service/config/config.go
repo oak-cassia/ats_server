e8469c44949ebae8560452a2d9f5a45452a494b6
oak_cassia <gnlckswjd1@gmail.com>
2025-02-23 20:14:07 +0900

    feat : add config

--- service/auth_service/config/config.go
+++ service/auth_service/config/config.go
@@ -3,27 +3,47 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
+	"redisclient"
+	_ "redisclient"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type AppConfig struct {
-	DB        *sql.DB
-	JWTSecret string
+	DB    *sql.DB
+	Redis *redisclient.RedisClient
+}
+
+func (ac *AppConfig) Close() {
+	_ = ac.DB.Close()
 }
 
 func LoadConfig() (*AppConfig, error) {
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbName := os.Getenv("DB_NAME")
-	jwtSecret := os.Getenv("JWT_SECRET")
-	if jwtSecret == "" {
-		jwtSecret = "defualt-secret"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
+
+	db, err := mysqlConfig()
+	redis := redisConfig()
+
+	return &AppConfig{
+		DB:    db,
+		Redis: redis,
+	}, nil
+}
+
+func mysqlConfig() (*sql.DB, error) {
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, host, name)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
@@ -37,8 +57,12 @@ func LoadConfig() (*AppConfig, error) {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 
-	return &AppConfig{
-		DB:        db,
-		JWTSecret: jwtSecret,
-	}, nil
+	return db, nil
+}
+
+func redisConfig() *redisclient.RedisClient {
+	host := os.Getenv("REDIS_HOST")
+	password := os.Getenv("REDIS_PASSWORD")
+	db := 0
+	return redisclient.NewRedisClient(host, password, db)
 }
