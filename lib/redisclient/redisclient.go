17e9d099925f17e4e04ce64d6e58d96a2accc716
oak_cassia <gnlckswjd1@gmail.com>
2025-02-23 20:11:50 +0900

    feat : add lib redisClient

---
+++ lib/redisclient/redisclient.go
@@ -0,0 +1,42 @@
+package redisclient
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(addr, password string, db int) *RedisClient {
+	client := redis.NewClient(&redis.Options{
+		Addr:         addr,
+		Password:     password,
+		DB:           db,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	})
+
+	return &RedisClient{
+		client: client,
+	}
+}
+
+func (r *RedisClient) SetData(key, value string, expire time.Duration) error {
+	ctx := context.Background()
+
+	return r.client.Set(ctx, key, value, expire).Err()
+}
+
+func (r *RedisClient) GetData(key string) (string, error) {
+	ctx := context.Background()
+	return r.client.Get(ctx, key).Result()
+}
+
+func GetSessionKey(email string) string {
+	return "session:" + email
+}
