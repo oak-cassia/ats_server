32e51c83cbf0499fde2754e76f439e9d4ccdf0a7
oak_cassia <gnlckswjd1@gmail.com>
2025-03-07 20:39:54 +0900

    feat: implement graceful server shutdown

--- service/auth_service/cmd/main.go
+++ service/auth_service/cmd/main.go
@@ -5,7 +5,10 @@ import (
 	"auth_service/internal/handler"
 	"auth_service/internal/repository"
 	"auth_service/internal/service"
+	"context"
+	"errors"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
 	"time"
@@ -15,6 +18,12 @@ const port = 10001
 const postMethod = "POST"
 
 func main() {
+	if err := run(context.Background()); err != nil {
+		log.Fatalf("failed to run: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
@@ -25,18 +34,40 @@ func main() {
 	authService := service.NewAuthService(cfg.DB, userRepo, cfg.Redis)
 	authHandler := handler.NewAuthHandler(authService)
 
-	http.HandleFunc("/register", Chain(
+	mux := http.NewServeMux()
+	mux.HandleFunc("/register", Chain(
 		authHandler.RegisterHandler,
 		Method(postMethod),
 		Timeout(5*time.Second),
 		TimeNow(),
 	))
-	http.HandleFunc("/login", Chain(
+	mux.HandleFunc("/login", Chain(
 		authHandler.LoginHandler,
 		Method(postMethod),
 		Timeout(5*time.Second),
 		TimeNow(),
 	))
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	s := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
+
+	// 반환 값으로 오류를 받을 수 없어서 errgroup 패키지를 사용하여 오류를 반환
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// 컨텍스트 취소 시 서버 종료
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
