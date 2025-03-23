50dd36271c179327a822052b9ad7d623cb524b64
oak_cassia <gnlckswjd1@gmail.com>
2025-03-23 22:41:24 +0900

    refactor: Interfaces in auth service and repository

--- service/auth_service/internal/repository/interface.go
+++ service/auth_service/internal/repository/interface.go
@@ -1,8 +1,15 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 type Queryer interface {
 	GetContext(ctx context.Context, dst interface{}, query string, args ...any) error
 	SelectContext(ctx context.Context, dst interface{}, query string, args ...any) error
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 }
--- service/auth_service/internal/service/interface.go
+++ service/auth_service/internal/service/interface.go
@@ -1,14 +1,19 @@
 package service
 
 import (
-	"context"
-
 	"auth_service/internal/model"
 	"auth_service/internal/repository"
+	"context"
+	"time"
 )
 
 type UserRepository interface {
 	GetUserByEmail(ctx context.Context, q repository.Queryer, email string) (*model.User, error)
 	CreateUser(ctx context.Context, exec repository.Execer, user *model.User) error
 	UpdateLastLogin(ctx context.Context, exec repository.Execer, user *model.User) error
+}
+
+type RedisClient interface {
+	Save(ctx context.Context, key, value string, expiration time.Duration) error
+	Delete(ctx context.Context, key string) error
 }
